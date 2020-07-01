54a1f6af6c3277e4c21bb6a3f9e988d1ab2b638d
kimycs <kimy.csjs@gmail.com>
2020-07-01 18:48:28 +0530

    add Some UI elements for HomeFragment

--- app/src/main/java/com/kimy/budgetbuddy/HomeActivity.java
+++ app/src/main/java/com/kimy/budgetbuddy/HomeActivity.java
@@ -8,14 +8,31 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import com.kimy.budgetbuddy.HomeFragment;
+import com.kimy.budgetbuddy.ExpenseFragment;
+import com.kimy.budgetbuddy.IncomeFragment;
+
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    private HomeFragment homeFragment;
+    private IncomeFragment incomeFragment;
+    private ExpenseFragment expenseFragment;
+
+    private FirebaseAuth csAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +43,13 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         toolbar.setTitle("Budget Buddy");
         setSupportActionBar(toolbar);
 
-
+        bottomNavigationView=findViewById(R.id.bottom_nav);
+        frameLayout=findViewById(R.id.main_frame);
         DrawerLayout drawerLayout=findViewById(R.id.drawer);
 
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
-
                 this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
-
         );
-
-
-
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
@@ -44,13 +57,45 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView=findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        homeFragment=new HomeFragment();
+        incomeFragment=new IncomeFragment();
+        expenseFragment=new ExpenseFragment();
 
+        setFragment(homeFragment);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
 
+                    case R.id.dashboard:
+                        setFragment(homeFragment);
+                        //bottomNavigationView.setItemBackgroundResource(R.color.home_bg);
+                        return true;
 
+                    case R.id.income:
+                        setFragment(incomeFragment);
+                        //bottomNavigationView.setItemBackgroundResource(R.color.income_bg);
+                        return true;
 
+                    case R.id.expense:
+                        setFragment(expenseFragment);
+                        //bottomNavigationView.setItemBackgroundResource(R.color.expense_bg);
+                        return true;
 
+                    default:
+                        return false;
 
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
 
     }
 
@@ -73,17 +118,24 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         switch (itemId){
 
             case R.id.dashboard:
+                fragment=new HomeFragment();
                 break;
 
             case R.id.income:
+                fragment=new IncomeFragment();
                 break;
 
             case R.id.expense:
+                fragment=new ExpenseFragment();
                 break;
+
+            /*case R.id.logout:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;*/
 
 
         }
-
 
         if (fragment!=null){
             FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
@@ -96,13 +148,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-    public void setSupportActionBar(Toolbar toolbar) {
-    }
-
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        
         displaySelectedListener(item.getItemId());
         return true;
     }
